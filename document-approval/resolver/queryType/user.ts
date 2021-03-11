a4af1d812dfa0d1df6488c28472247626c1f1675
y0on2q <ltnscp9028@gmail.com>
2021-03-12 02:04:52 +0900

    nexus(field) - jsonwebtoken을 이용한 로그인기능 구현

--- document-approval/resolver/queryType/user.ts
+++ document-approval/resolver/queryType/user.ts
@@ -1,9 +1,23 @@
-import { queryType } from 'nexus';
+import { nonNull, queryType, stringArg } from 'nexus';
+import { generateToken } from '../../generated/util/auth';
 
 const user = queryType({
   definition(t) {
     t.crud.user();
     t.crud.users();
+    t.field('login', {
+      type: 'authPayload',
+      args: {
+        email: nonNull(stringArg()),
+      },
+      async resolve(root, { email }, { prisma }) {
+        const user = await prisma.user.findUnique({ where: { email: email } });
+        if (user === null) {
+          return { token: null, user: null };
+        }
+        return { token: generateToken(user), user };
+      },
+    });
   },
 });
 
---
+++ document-approval/types/authUserInfo.ts
@@ -0,0 +1,5 @@
+export interface authUserInfo {
+  name: string;
+  email: string;
+  user_id: number;
+}
